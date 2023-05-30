e025475744ee7cc1d9765f00fa260b7a108fb44f
Ollan Muza <128623143+kxngollan@users.noreply.github.com>
2023-05-30 10:21:59 +0100

    First Version

---
+++ index.js
@@ -0,0 +1,17 @@
+
+  const scriptURL = 'https://script.google.com/macros/s/AKfycbyPaR6pOTIcn12V85T_wm1xff05t1KIIbG7IVgmBvCGOjCO7lFw9VdbR_w97j721J8l-g/exec'
+  const form = document.forms['contactForm']
+  const success = document.getElementById("success")
+  const failure = document.getElementById("failure")
+
+  form.addEventListener('submit', e => {
+    e.preventDefault()
+    fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+      .then(response => {
+        form.reset();
+        success.innerHTML = "Thank you! Message Successfully sent, will be in touch soon."
+      })
+      .catch(error => {
+        failure.innerText = "Error! Message failed to send. If issue carries on happening, please email: Onlymuza@gmail.com"
+      })
+  })
